7aec8537374846666e0db97d094a0234588a5ab6
Dinh Tien <82441050+dinhtiendev@users.noreply.github.com>
2023-02-18 17:24:17 +0700

    Add front-end include Login, Forget Password, Home and Profile

---
+++ StudentAttandance/Pages/User/Profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentAttandance.Pages.User
+{
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
