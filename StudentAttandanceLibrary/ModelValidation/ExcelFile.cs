e8825d82e2d2b61f3a33d14d14c1783a5b6795ee
Nguyen Tien Dat (K15 HL) <datntls15062001@gmail.com>
2023-02-19 21:06:23 +0700

    up

---
+++ StudentAttandanceLibrary/ModelValidation/ExcelFile.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentAttandanceLibrary.ModelValidation
+{
+    public class ExcelFile
+    {
+        [Required]
+        public string File { get; set; }
+    }
+}
