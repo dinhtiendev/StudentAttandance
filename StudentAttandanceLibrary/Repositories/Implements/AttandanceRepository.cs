59ffb38662b994ee1c16d778bb744182d5a92555
Dinh Tien <82441050+dinhtiendev@users.noreply.github.com>
2023-03-01 22:31:17 +0700

    Add View Schedule and Attandance Repos

--- StudentAttandanceLibrary/Repositories/Implements/AttandanceRepository.cs
+++ StudentAttandanceLibrary/Repositories/Implements/AttandanceRepository.cs
@@ -5,11 +5,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StudentAttandanceLibrary.Models;
 
 namespace StudentAttandanceLibrary.Repositories.Implements
 {
     public class AttandanceRepository : IAttandanceRepository
     {
+        StudentAttendanceManagementContext context = new StudentAttendanceManagementContext();
+
         public AttandanceDto GetAttandanceById(int id)
         {
             throw new NotImplementedException();
@@ -20,9 +23,33 @@ namespace StudentAttandanceLibrary.Repositories.Implements
             throw new NotImplementedException();
         }
 
-        public List<AttandanceDto> GetAttandancesByWeekAndStudentId(DateTime startDate, DateTime endDate, string StudentId)
+        public List<AttandanceDto> GetAttandancesByWeekAndStudentId(DateTime startDate, DateTime endDate, string studentId)
         {
-            throw new NotImplementedException();
+            var query = (from session in context.Sessions
+                         join attendance in context.Attendances
+                         on session.SessionId equals attendance.SessionId
+                         join timeSlot in context.TimeSlots
+                         on session.TimeSlotId equals timeSlot.TimeSlotId
+                         join room in context.Rooms
+                         on session.RoomId equals room.RoomId
+                         join groups in context.Groups
+                         on session.GroupId equals groups.GroupId
+                         join teacher in context.Teachers
+                         on session.TeacherId equals teacher.TeacherId
+                         where attendance.StudentId == studentId && session.Date >= startDate && session.Date <= endDate
+                         select new AttandanceDto
+                         {
+                             SessionId = session.SessionId,
+                             AttendanceId = attendance.AttendanceId,
+                             Date = session.Date,
+                             Index = session.Index,
+                             Attanded = session.Attanded,
+                             TeacherName = teacher.UserName,
+                             TimeSlot = new TimeSlot { TimeSlotId = timeSlot.TimeSlotId, Description = timeSlot.Description },
+                             GroupName = groups.GroupName,
+                             Present = attendance.Present
+                         }).ToList();
+            return query;
         }
 
         public void UpdateAttandances(List<AttandanceDto> attandances)
